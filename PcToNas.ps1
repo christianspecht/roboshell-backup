e83e53f814fc8a5bb02266ce5e5a56be28d6ca63
Christian Specht <christi@nspecht.de>
2011-04-30 21:41:08 +0200

    First commit
    - copy local folders to NAS
    - Parameter /xj to exclude junctions (in Win7 user folder, for example)

---
+++ PcToNas.ps1
@@ -0,0 +1,21 @@
+$config = [xml](get-content .\Config.xml)
+
+$nasdrive = $config.copy.nasdrive.value
+$nasfolder = $nasdrive + $config.copy.nasfolder.value
+
+write-host '##########################################################'
+write-host '#'
+write-host '#  Backup from local machine to NAS'
+write-host '#'
+write-host '#  Folders will be backed up to:'
+write-host '# '$nasfolder
+write-host '#'
+write-host '##########################################################'
+write-host ''
+
+foreach ($node in $config.copy.sourcefolder)
+{
+    $from = $node.from
+    $to = $nasfolder + $node.to   
+    robocopy.exe "$from" "$to" *.* /mir /xj
+}
