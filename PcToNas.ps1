932eeb7825ce085b6226ccfe2ee989e6f12a315a
Christian Specht <christi@nspecht.de>
2011-04-30 22:18:42 +0200

    change all outputs to double quotes
    http://www.computerperformance.co.uk/powershell/powershell_quotes.htm#Example_2_Double_Quotes_

--- PcToNas.ps1
+++ PcToNas.ps1
@@ -3,15 +3,15 @@ $config = [xml](get-content .\Config.xml)
 $nasdrive = $config.copy.nasdrive.value
 $nasfolder = $nasdrive + $config.copy.nasfolder.value
 
-write-host '##########################################################'
-write-host '#'
-write-host '#  Backup from local machine to NAS'
-write-host '#'
-write-host '#  Folders will be backed up to:'
-write-host '# '$nasfolder
-write-host '#'
-write-host '##########################################################'
-write-host ''
+write-host "##########################################################"
+write-host "#"
+write-host "#  Backup from local machine to NAS"
+write-host "#"
+write-host "#  Folders will be backed up to:"
+write-host "#  $nasfolder"
+write-host "#"
+write-host "##########################################################"
+write-host ""
 
 foreach ($node in $config.copy.sourcefolder)
 {
