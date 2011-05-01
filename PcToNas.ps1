95bb6a446e2b42e85f92c8ae595b339f96c0e16c
Christian Specht <christi@nspecht.de>
2011-05-01 18:51:44 +0200

    add wait time after displaying messages

--- PcToNas.ps1
+++ PcToNas.ps1
@@ -2,6 +2,7 @@ $config = [xml](get-content .\Config.xml)
 
 $nasdrive = $config.copy.nasdrive.value
 $nasfolder = $nasdrive + $config.copy.nasfolder.value
+$waittime = $config.copy.waittime.value
 
 write-host "##########################################################"
 write-host "#"
@@ -10,8 +11,11 @@ write-host "#"
 write-host "#  Folders will be backed up to:"
 write-host "#  $nasfolder"
 write-host "#"
+write-host "#  Backup will start in $waittime seconds!"
+write-host "#"
 write-host "##########################################################"
 write-host ""
+start-sleep -s $waittime
 
 foreach ($node in $config.copy.sourcefolder)
 {
