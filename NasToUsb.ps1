349c95b2b691f1a27bb6306a84c9a66d33412bdb
Christian Specht <christi@nspecht.de>
2011-05-01 22:25:09 +0200

    check if NAS and USB drives exist

--- NasToUsb.ps1
+++ NasToUsb.ps1
@@ -2,6 +2,36 @@ $config = [xml](get-content .\Config.xml)
 
 $nasdrive = $config.copy.nasdrive.value
 $usbdrive = $config.copy.usbdrive.value
+$waittime = $config.copy.waittime.value
+
+if (!(test-path $nasdrive))
+{
+	write-host "##########################################################"
+	write-host "#"
+	write-host "#  NAS drive $nasdrive does not exist!"
+	write-host "#  Backup to external USB drive will not run!"
+	write-host "#"
+	write-host "#  This window will close in $waittime seconds!"
+	write-host "#"
+	write-host "##########################################################"
+	start-sleep -s $waittime
+	break
+}
+
+if (!(test-path $usbdrive))
+{
+	write-host "##########################################################"
+	write-host "#"
+	write-host "#  USB drive $usbdrive does not exist!"
+	write-host "#  Backup to external USB drive will not run!"
+	write-host "#"
+	write-host "#  This window will close in $waittime seconds!"
+	write-host "#"
+	write-host "##########################################################"
+	start-sleep -s $waittime
+	break
+}
+
 $usbfolder = $usbdrive + "Backup_NAS"
 
 write-host "##########################################################"
