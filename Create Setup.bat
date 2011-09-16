160e76c08f8292e154a7dfb37b2e524b8783e4e0
Christian Specht <christi@nspecht.de>
2011-09-16 18:38:10 +0200

    include product version in product name and MSI file name
    code from http://stackoverflow.com/q/1688569#1688819

--- Create Setup.bat
+++ Create Setup.bat
@@ -1,9 +1,11 @@
+set productversion=1.0
+
 candle wix\roboshell-backup.wxs
 
-light -ext WixUIExtension roboshell-backup.wixobj
+light -ext WixUIExtension roboshell-backup.wixobj  -out roboshell-backup-%productversion%.msi
 
 del /q release\*.*
 md release
-copy roboshell-backup.msi release
+copy roboshell-backup-%productversion%.msi release
 
 pause
