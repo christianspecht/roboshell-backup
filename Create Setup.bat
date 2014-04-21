85eecb686b42364407b7ceff7fd61e76c245e006
Christian Specht <christi@nspecht.de>
2014-04-21 21:52:37 +0200

    create setup AND zip file

--- Create Setup.bat
+++ Create Setup.bat
@@ -5,8 +5,15 @@ call wix\version-number.bat
 
 del /q release\*.*
 
+rem MSI
+
 candle wix\roboshell-backup.wxs
 
-light -ext WixUIExtension roboshell-backup.wixobj  -out release\roboshell-backup-%productversion%.msi
+light -ext WixUIExtension roboshell-backup.wixobj  -out release\msi\roboshell-backup-%productversion%.msi
+
+
+rem ZIP
+%~dp0\..\7za.exe a -tzip release\zip\roboshell-backup-%productversion%.zip @zipfiles.txt
+
 
 pause
