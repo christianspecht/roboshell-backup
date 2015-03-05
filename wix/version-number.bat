4ad4e3d9a080e7b1bb99f728fd79fff343925851
Christian Specht <christi@nspecht.de>
2015-03-05 23:02:40 +0100

    update version

--- wix/version-number.bat
+++ wix/version-number.bat
@@ -1,7 +1,7 @@
 rem Before releasing a new version, please change BOTH values as described below
 
 rem This is the version number for the setup. Just increase it.
-set productversion=1.2
+set productversion=1.3
 
 rem Replace this GUID by a new one. Important: must be in capital letters and with curly braces!
-set ProductId={ECCB6495-32F9-405d-98E0-8001AB5C5985}
+set ProductId={28E6C2A7-509D-4B43-A654-EB0D0EBB5F68}
